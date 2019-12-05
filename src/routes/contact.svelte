c2b030c71716eda679b2a37ee2004fa5a47cf468
Daniel Sokil <dtechtours@gmail.com>
2019-12-04 19:53:07 -0500

    Testing Netlify Form

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,5 +1,43 @@
+<style>
+  form {
+    margin: 2rem;
+  }
+
+  form input,
+  form textarea {
+    display: block;
+  }
+</style>
+
 <svelte:head>
   <title>Contact | European Market</title>
 </svelte:head>
 
 <h1>Contact</h1>
+
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>
+      Your Name:
+      <input type="text" name="name" />
+    </label>
+  </p>
+
+  <p>
+    <label>
+      Your Email:
+      <input type="email" name="email" />
+    </label>
+  </p>
+
+  <p>
+    <label>
+      Message:
+      <textarea name="message" />
+    </label>
+  </p>
+
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
