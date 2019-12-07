1f956a0292c0cc122d4ce1bdfbabbaec915e761e
Daniel Sokil <dtechtours@gmail.com>
2019-12-06 20:36:21 -0500

    Decrease PNG Compression Level

--- src/_utils/image.js
+++ src/_utils/image.js
@@ -1,7 +1,7 @@
 export function imageCDN(path, options = "") {
   if (typeof document === "object") {
     if (!location.origin.includes(":8000")) {
-      return `https://images.weserv.nl/?url=${location.origin}${path}&il${options}`;
+      return `https://images.weserv.nl/?url=${location.origin}${path}&l=0&il${options}`;
     } else return path;
   } else {
     // https://stackoverflow.com/a/19126281
