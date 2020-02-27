1dcd6cb73e17056a590127a21a9e682bac126f9f
s0kil <dtechtours@gmail.com>
2020-02-26 22:46:59 -0500

    Switch to Rollup Bundler

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,20 @@
-import resolve from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
-import commonjs from "@rollup/plugin-commonjs";
-import svelte from "rollup-plugin-svelte";
 import babel from "rollup-plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
 import config from "sapper/config/rollup.js";
 import pkg from "./package.json";
+import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+import svelte from "rollup-plugin-svelte";
+import { terser } from "rollup-plugin-terser";
 
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const onwarn = (warning, onwarn) => (warning.code === "CIRCULAR_DEPENDENCY" && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+const onwarn = (warning, onwarn) =>
+  (warning.code === "CIRCULAR_DEPENDENCY" &&
+    /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  onwarn(warning);
 
 export default {
   client: {
@@ -33,31 +36,36 @@ export default {
       }),
       commonjs(),
 
-      legacy && babel({
-        extensions: [".js", ".mjs", ".html", ".svelte"],
-        runtimeHelpers: true,
-        exclude: ["node_modules/@babel/**"],
-        presets: [
-          ["@babel/preset-env", {
-            targets: {
-              browsers: [
-                "last 10 versions",
-                "ie >= 11"
-              ]
-            }
-          }]
-        ],
-        plugins: [
-          "@babel/plugin-syntax-dynamic-import",
-          ["@babel/plugin-transform-runtime", {
-            useESModules: true
-          }]
-        ]
-      }),
+      legacy &&
+        babel({
+          extensions: [".js", ".mjs", ".html", ".svelte"],
+          runtimeHelpers: true,
+          exclude: ["node_modules/@babel/**"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["last 10 versions", "ie >= 11"]
+                }
+              }
+            ]
+          ],
+          plugins: [
+            "@babel/plugin-syntax-dynamic-import",
+            [
+              "@babel/plugin-transform-runtime",
+              {
+                useESModules: true
+              }
+            ]
+          ]
+        }),
 
-      !dev && terser({
-        module: true
-      })
+      !dev &&
+        terser({
+          module: true
+        })
     ],
 
     onwarn
@@ -81,7 +89,8 @@ export default {
       commonjs()
     ],
     external: Object.keys(pkg.dependencies).concat(
-      require("module").builtinModules || Object.keys(process.binding("natives"))
+      require("module").builtinModules ||
+        Object.keys(process.binding("natives"))
     ),
 
     onwarn
