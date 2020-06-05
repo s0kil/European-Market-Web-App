b4774411a73edb3a73ffcd36fef1a44d5fe379b7
Daniel Sokil <dtechtours@gmail.com>
2020-06-04 22:58:27 -0400

    Added Script To Generate Server Push Headers For Client JS File

---
+++ scripts/serverPushHeaders.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CLIENT_JS_FILE=$(cd __sapper__/export/ && find client -maxdepth 1 -iname "client.*.js" || exit)
+
+HEADERS="
+/*.html
+ Link: </$CLIENT_JS_FILE>; rel=preload; as=script
+"
+
+# https://unix.stackexchange.com/a/274709
+echo "$HEADERS" | awk NF >__sapper__/export/_headers
+
+echo "Generated Server Push Headers For: $CLIENT_JS_FILE"
