88ca72ad5d11b4d124ad75b271f6d28865325686
Daniel Sokil <dtechtours@gmail.com>
2020-06-05 14:17:03 -0400

    Gnerate serverPushHeaders On Production Build

--- scripts/serverPushHeaders.sh
+++ scripts/serverPushHeaders.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-CLIENT_JS_FILE=$(cd __sapper__/export/ && find client -maxdepth 1 -iname "client.*.js" || exit)
+CLIENT_JS_FILE=$(cd __sapper__/export/ && find client -maxdepth 1 -iname "client.*.js")
+if test -z "$CLIENT_JS_FILE"; then
+  exit 1
+fi
 
 HEADERS="
 /
- Link: </$CLIENT_JS_FILE>; rel=preload; as=script; crossorigin
+ Link: </$CLIENT_JS_FILE>; rel=modulepreload;
 "
 
 # https://unix.stackexchange.com/a/274709
