326ab0eaabe3558d704de401f81249fa5d39c52d
Daniel Sokil <dtechtours@gmail.com>
2020-08-08 02:24:45 -0400

    Upgrade Sapper To 0.28 (#55)
    * Upgrade Sapper To 0.28
    
    * Subfont Disable Dry Run

--- rollup.config.js
+++ rollup.config.js
@@ -12,6 +12,7 @@ const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
+  (warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
   (warning.code === "CIRCULAR_DEPENDENCY" &&
     /[/\\]@sapper[/\\]/.test(warning.message)) ||
   onwarn(warning);
@@ -84,6 +85,7 @@ export default {
       }),
       svelte({
         generate: "ssr",
+        hydratable: true,
         dev,
       }),
       resolve({
